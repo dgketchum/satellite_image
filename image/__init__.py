faf9c8c463c9e0683aa6c40a40300fac40406234
dgketchum <dgketchum@gmail.com>
2017-04-28 13:00:33 -0600

    test instantiation

--- image/__init__.py
+++ image/__init__.py
@@ -1,4 +1,4 @@
-# ===============================================================================
+# =============================================================================================
 # Copyright 2017 dgketchum
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,27 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ===============================================================================
+# =============================================================================================
 
-import unittest
-import pkg_resources
-
-from image.image import SatelliteImage
-
-class ImageTestCase(unittest.TestCase):
-    def setUp(self):
-        self.invalid_string = 'blah'
-        self.valid_file =
-        self.valid_dir = None
-
-    def tearDown(self):
-        pass
-
-    def test_instantiate(self):
-
+import os
 
 
 if __name__ == '__main__':
-    unittest.main()
+    home = os.path.expanduser('~')
 
-# ===============================================================================
+# =============================================================================================
