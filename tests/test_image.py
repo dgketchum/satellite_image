a03c924a5274e368914e67a1e2b9634ae6350df3
dgketchum <dgketchum@gmail.com>
2018-03-02 15:01:46 -0700

    changed test paths to root level at .tests.../data(root) for some reason wasn't working from /satellite_image (top) level anymore

--- tests/test_image.py
+++ tests/test_image.py
@@ -26,7 +26,7 @@ from sat_image.image import LandsatImage, Landsat5, Landsat7, Landsat8
 
 class LandsatImageTestCase(unittest.TestCase):
     def setUp(self):
-        self.dir_name_LT5 = 'satellite_image/tests/data/image_test/lt5_image'
+        self.dir_name_LT5 = 'data/image_test/lt5_image'
 
     def test_earth_sun(self):
         landsat = LandsatImage(self.dir_name_LT5)
@@ -48,10 +48,10 @@ class LandsatImageTestCase(unittest.TestCase):
 
 class Landsat5TestCase(unittest.TestCase):
     def setUp(self):
-        self.dir_name_LT5 = 'satellite_image/tests/data/image_test/lt5_image'
+        self.dir_name_LT5 = 'data/image_test/lt5_image'
         # results from fmask.exe
         # bitbucket.org/chchrsc/python-fmask/
-        self.exp_reflect = 'satellite_image/tests/data/image_test/lt5_image/LT5_reflct_10000x_b1.tif'
+        self.exp_reflect = 'data/image_test/lt5_image/LT5_reflct_10000x_b1.tif'
         self.l5 = Landsat5(self.dir_name_LT5)
         self.cell = 150, 150
 
@@ -127,8 +127,8 @@ class Landsat7TestCase(unittest.TestCase):
     def setUp(self):
         # results from fmask.exe
         # bitbucket.org/chchrsc/python-fmask/
-        self.dir_name_LT7 = 'satellite_image/tests/data/image_test/le7_image'
-        self.exp_reflect = 'satellite_image/tests/data/image_test/le7_image/LE7_reflct_10000x_b1.tif'
+        self.dir_name_LT7 = 'data/image_test/le7_image'
+        self.exp_reflect = 'data/image_test/le7_image/LE7_reflct_10000x_b1.tif'
         self.l7 = Landsat7(self.dir_name_LT7)
         self.cell = 300, 300
 
@@ -195,7 +195,7 @@ class Landsat7TestCase(unittest.TestCase):
 
 class Landsat8TestCase(unittest.TestCase):
     def setUp(self):
-        self.dirname_cloud = 'satellite_image/tests/data/image_test/lc8_image'
+        self.dirname_cloud = 'data/image_test/lc8_image'
         # results from rio-toa
         self.ex_bright = os.path.join(self.dirname_cloud, 'LC8_brightemp_B10.TIF')
         self.ex_reflect = os.path.join(self.dirname_cloud, 'LC8_reflct_B1.TIF')
